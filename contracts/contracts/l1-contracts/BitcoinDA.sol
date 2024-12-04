86a43d744c0f7b4b8311c27998438bff02d367f4
jagdeep sidhu <sidhujag@syscoin.org>
2024-12-03 21:57:01 -0800

    update bitcoinda to be rollup mode instead of validium

--- contracts/contracts/l1-contracts/BitcoinDA.sol
+++ contracts/contracts/l1-contracts/BitcoinDA.sol
@@ -9,21 +9,47 @@ import {IL1DAValidator, L1DAValidatorOutput} from "./IL1DAValidator.sol";
 contract BitcoinDAL1DAValidator is IL1DAValidator {
     function checkDA(
         uint256, // _chainId
-        bytes32, // _l2DAValidatorOutputHash
+        bytes32 _l2DAValidatorOutputHash,
         bytes calldata _operatorDAInput,
         uint256 maxBlobsSupported
     ) external override returns (L1DAValidatorOutput memory output) {
-        // For BitcoinDA, we expect the operator to just provide the data for us.
-        // We don't need to do any checks with regard to the l2DAValidatorOutputHash.
-        require(_operatorDAInput.length == 32);
-
-        bytes32 stateDiffHash = abi.decode(_operatorDAInput, (bytes32));
-        _verifyBitcoinDA(stateDiffHash);
+        {
+            (
+               bytes32 uncompressedStateDiffHash,
+               bytes32 fullPubdataHash,
+            ) = _processL2RollupDAValidatorOutputHash(_l2DAValidatorOutputHash, _operatorDAInput);
+            // circuit will commit to the uncompressed diff hash but we related the compressed to the uncompressed one via validatePubData and recomputed the hash to check here. 
+            output.stateDiffHash = uncompressedStateDiffHash;
+            // fullPubdataHash should include user_logs, l2_to_l1_messages, published_bytecodes, compressed state_diffs
+            _verifyBitcoinDA(fullPubdataHash);
+        }
         // The rest of the fields that relate to blobs are empty.
-        output.stateDiffHash = stateDiffHash;
-
         output.blobsLinearHashes = new bytes32[](maxBlobsSupported);
         output.blobsOpeningCommitments = new bytes32[](maxBlobsSupported);
+    }
+
+    function _processL2RollupDAValidatorOutputHash(
+        bytes32 _l2DAValidatorOutputHash,
+        bytes calldata _operatorDAInput
+    )
+        internal
+        pure
+        returns (
+            bytes32 uncompressedStateDiffHash,
+            bytes32 fullPubdataHash
+        )
+    {
+        // Ensure the operator DA input is at least 32 bytes long
+        require(_operatorDAInput.length >= 32, "Operator DA input too small");
+        // we are isolating fullPubdataHash to ensure its correctness here by checking state diff(validated) + full data hash(unvalidated) == l2 DA validator output(validated)
+        // fullPubdataHash is used as DA and commits to the compressed state diff (zk circuit input)
+        uncompressedStateDiffHash = bytes32(_operatorDAInput[:32]);
+        fullPubdataHash = bytes32(_operatorDAInput[32:64]);
+
+        // Now, we need to double check that the provided input was indeed returned by the L2 DA validator.
+        if (keccak256(_operatorDAInput[:64]) != _l2DAValidatorOutputHash) {
+            revert InvalidL2DAOutputHash();
+        }
     }
 
     function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
--- contracts/contracts/l2-contracts/BitcoinDA.sol
+++ contracts/contracts/l2-contracts/BitcoinDA.sol
@@ -4,26 +4,51 @@ pragma solidity 0.8.24;
 
 // solhint-disable gas-custom-errors, reason-string
 
-import {IL2DAValidator} from "./interfaces/IL2DAValidator.sol";
+import {IL2DAValidator} from "../interfaces/IL2DAValidator.sol";
+import {StateDiffL2DAValidator} from "./StateDiffL2DAValidator.sol";
+
+import {EfficientCall} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/EfficientCall.sol";
+ort {ReconstructionMismatch, PubdataField} from "./DAErrors.sol";
 
 /// BitcoinDA validator. It will publish inclusion data that would allow to verify the inclusion.
-contract BitcoinDAL2DAValidator is IL2DAValidator {
+contract BitcoinDAL2DAValidator is IL2DAValidator, StateDiffL2DAValidator {
     function validatePubdata(
         // The rolling hash of the user L2->L1 logs.
-        bytes32,
+        bytes32 _chainedLogsHash,
         // The root hash of the user L2->L1 logs.
         bytes32,
         // The chained hash of the L2->L1 messages
-        bytes32,
+        bytes32 _chainedMessagesHash,
         // The chained hash of uncompressed bytecodes sent to L1
-        bytes32,
+        bytes32 _chainedBytescodesHash,
         // Operator data, that is related to the DA itself
         bytes calldata _totalL2ToL1PubdataAndStateDiffs
-    ) external pure returns (bytes32 outputHash) {
-        // Since we do not need to publish anything to L1, we can just return 0.
-        // Note, that Rollup validator sends the hash of uncompressed state diffs, since the
-        // correctness of the publish pubdata depends on it. However Validium doesn't sent anything,
-        // so we don't need to publish even that.
-        outputHash = bytes32(0);
+    ) external returns (bytes32 outputHash) {
+        (bytes32 uncompressedStateDiffHash, bytes calldata _totalPubdata, bytes calldata leftover) = _produceStateDiffPubdata(
+            _chainedLogsHash,
+            _chainedMessagesHash,
+            _chainedBytescodesHash,
+            _totalL2ToL1PubdataAndStateDiffs
+        );
+
+        /// Check for calldata strict format
+        if (leftover.length != 0) {
+            revert ReconstructionMismatch(
+                PubdataField.ExtraData,
+                bytes32(leftover.length + _totalL2ToL1PubdataAndStateDiffs.length),
+                bytes32(_totalL2ToL1PubdataAndStateDiffs.length)
+            );
+        }
+
+        // The preimage under the hash `outputHash` is expected to be in the following format:
+        // - First 32 bytes are the hash of the uncompressed state diff.
+        // - Then, there is a 32-byte hash of the DA.
+
+        outputHash = keccak256(
+            abi.encodePacked(
+                uncompressedStateDiffHash,
+                EfficientCall.keccak(_totalPubdata)
+            )
+        );
     }
 }
