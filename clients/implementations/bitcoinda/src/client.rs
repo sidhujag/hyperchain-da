7556653dd31e81e8c192e043a8e33ba2634e8a17
jagdeep sidhu <sidhujag@syscoin.org>
2024-12-05 10:18:04 -0800

    clean up

--- clients/implementations/bitcoinda/src/client.rs
+++ clients/implementations/bitcoinda/src/client.rs
@@ -1,10 +1,10 @@
 use da_config::bitcoinda::BitcoinDAConfig;
 use async_trait::async_trait;
 use std::fmt::{Debug, Formatter};
+use anyhow::Context;
 
 use zksync_da_client::{types, DataAvailabilityClient};
 use zksync_env_config::FromEnv;
-
 use zksync_types::web3::keccak256;
 
 #[derive(Clone)]
@@ -14,10 +14,11 @@ pub struct BitcoinDAClient {
 }
 
 impl BitcoinDAClient {
-    const MAX_BLOB_SIZE: usize = 2 * 1024 * 1024; // 2 mb
+    const MAX_BLOB_SIZE: usize = 2 * 1024 * 1024; // 2 MB
+
     pub fn new() -> anyhow::Result<Self> {
-        // TODO: read proto config first
-        let config = BitcoinDAConfig::from_env()?;
+        let config = BitcoinDAConfig::from_env()
+            .context("Failed to load BitcoinDAConfig from environment")?;
 
         Ok(Self {
             light_node_url: config.api_node_url,
@@ -33,27 +34,35 @@ impl DataAvailabilityClient for BitcoinDAClient {
         _: u32, // batch_number
         data: Vec<u8>,
     ) -> Result<types::DispatchResponse, types::DAError> {
-        // hash the blob
+        if data.len() > Self::MAX_BLOB_SIZE {
+            return Err(types::DAError::InvalidData(
+                "Blob size exceeds the maximum limit".to_string(),
+            ));
+        }
+
+        // Hash the blob
         let hash = keccak256(&data);
-        // send data to syscoin client, await its confirmation via txid
-        Ok(DispatchResponse {
-            blob_id: hash
-        })
+
+        // TODO: Send data to Syscoin client and await confirmation
+        Ok(types::DispatchResponse { blob_id: hash })
     }
+
     async fn get_inclusion_data(
         &self,
         _blob_id: &str,
     ) -> anyhow::Result<Option<types::InclusionData>, types::DAError> {
-        // return the hash of the pubdata which accompanied with the uncompressed state diff hash should enable us to recreate output hash from validatePubData
-        Ok(Some(InclusionData { data: _blob_id.as_bytes().to_vec() }))
+        // Return the hash of the pubdata
+        Ok(Some(types::InclusionData {
+            data: _blob_id.as_bytes().to_vec(),
+        }))
     }
 
     fn clone_boxed(&self) -> Box<dyn DataAvailabilityClient> {
         Box::new(self.clone())
     }
 
-    fn blob_size_limit(&self) ->  Option<usize> {
-        Some(BitcoinDAClient::MAX_BLOB_SIZE)
+    fn blob_size_limit(&self) -> Option<usize> {
+        Some(Self::MAX_BLOB_SIZE)
     }
 }
 
